2661fdb7f8d09a4538823ec7ad7e76d2b82da7fa
edwin <edwinkip@hotmail.com>
2026-03-09 21:09:00 +0100

    basic flask code

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,5 @@
-import from flask import Flask
+from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return '<p>Hello, World!</p>'
+    return '<p>vandaag is een mooie dag</p>'
